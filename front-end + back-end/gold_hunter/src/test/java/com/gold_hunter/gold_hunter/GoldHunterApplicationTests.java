ced5b0b305ee7357337bfdcf808fe93a624852f9
Fopix <57790370+Fopix@users.noreply.github.com>
2025-02-03 13:09:54 +0300

    Add files via upload

---
+++ front-end + back-end/gold_hunter/src/test/java/com/gold_hunter/gold_hunter/GoldHunterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gold_hunter.gold_hunter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoldHunterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
